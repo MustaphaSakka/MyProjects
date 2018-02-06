7b6a61ffea4a341d855319d64d531ff112e2fbb7
MustaphaSakka <sakka.mustaphaa@gmail.com>
2018-02-06 20:52:10 +0100

    Update files

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -1,21 +1,17 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 var canvas, color;
 canvas = $('#pixel_canvas');
 color = "#000000";
 
 function makeGrid() {
-	var heightVal, widthVal;
-	
+	let heightVal, widthVal;
+	// Set the size of the table
 	heightVal = $('#input_height').val();
 	widthVal = $('#input_width').val();
 	
+	// Build Grid
 	for (var row = 0; row < heightVal; row++)
 	{
-		var horizantalRow = $('<tr></tr>');
+		let horizantalRow = $('<tr></tr>');
 		canvas.append(horizantalRow);
 		for (var cell = 0; cell < widthVal; cell++)
 		{
@@ -26,27 +22,28 @@ function makeGrid() {
 	setColorToSquare();
 }
 
+// This function delete previous existing Canvas to restart a new canvas
 function deletePreviousCanvas(){
-	var tr;
+	let tr;
 	tr = canvas.find('tr');
 	tr.remove();
 }
 
-function setColorToSquare(){
-	
+function setColorToSquare(){	
 	// Set the new choosen color
 	$('#colorPicker').change(function(){
 		color = $(this).val();
 	})
 
-	$('#pixel_canvas').find('tr').on('click', 'td', function(event){
+	// Fill the cell clicked with the chosen color
+	canvas.find('tr').on('click', 'td', function(event){
 		$(event.target).css('background-color', color);
 	})
 }
 
 $(function() {
-	var td, tr;
-	var verticalRow = $('<td></td>');
+	let td, tr, verticalRow;
+	verticalRow = $('<td></td>');
 	tr = canvas.find('tr');
 
 	$('#sizePicker').submit(function(event) {
